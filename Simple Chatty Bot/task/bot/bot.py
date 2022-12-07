3a655b51ae9d002cb1c1bdd130a83f09adb7cd44
Acestus <architect@acestus.com>
2022-12-06 23:38:52 -0600

    Initial Commit

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,5 +1,57 @@
-# write your code here
-msg_1 = " Hello! My name is Bob.\nI was created in 2020."
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
 
-print(msg_1)
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    print("Why do fingers wrinkle in water?")
+    print("1. Because they are wet.")
+    print("2. Because they are cold.")
+    print("3. Because they are old.")
+    print("4. Because they are wrinkly.")
+    answer = int(input())
+    while answer != 2:
+        print("Please, try again.")
+        answer = int(input())
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
